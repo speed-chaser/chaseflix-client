23ed68ba9ed5ccfe30d268c253c61ae8ea91db8a
speed-chaser <aychasepro@gmail.com>
2023-07-19 16:50:34 -0400

    Revert "stashed changes update"
    This reverts commit 4458f175c9c20b7788d7bf0174411c1f78812ec7.

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -32,6 +32,7 @@ export const ProfileView = ({ token, user, movies, setUser }) => {
   const handleShowUpdateModal = () => setShowUpdateModal(true);
 
   useEffect(() => {
+<<<<<<< Updated upstream
     if (!profileUsername) return;
     console.log("Username:", profileUsername);
     fetch(
@@ -43,6 +44,16 @@ export const ProfileView = ({ token, user, movies, setUser }) => {
         },
       }
     )
+=======
+    if (!Username) return;
+    console.log("Username:", Username);
+    fetch(`https://chaseflix-481df0d77a4b.herokuapp.com/users/${Username}`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+>>>>>>> Stashed changes
       .then((response) => {
         if (response.ok) return response.json();
         throw new Error("User not found");
@@ -224,9 +235,17 @@ export const ProfileView = ({ token, user, movies, setUser }) => {
           <div>Birthday: {viewedUser && formattedDate}</div>
           {isOwnProfile && (
             <>
+<<<<<<< Updated upstream
               <Button variant="primary" onClick={handleShowUpdateModal}>
                 Edit profile
               </Button>
+=======
+              <Form method="get" action={`/users/${user.username}/update`}>
+                <Button variant="primary" type="submit">
+                  Edit profile
+                </Button>
+              </Form>
+>>>>>>> Stashed changes
               <br />
               <br />
               <Button className="mb-3" variant="danger" onClick={handleShow}>
